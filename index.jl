45d2511c92fa9fba563ed90e0961478840af3936
Connor Burns <ctrekker4@gmail.com>
2021-02-15 18:20:36 -0700

    Add cellular automata nb

---
+++ index.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f1d92cd0-6ff3-11eb-1949-c3ff2812f7d0
+md"# Cotangent Research"
+
+# ╔═╡ 14757f00-6ff4-11eb-1af8-e179b5503070
+md"Hello there"
+
+# ╔═╡ Cell order:
+# ╟─f1d92cd0-6ff3-11eb-1949-c3ff2812f7d0
+# ╟─14757f00-6ff4-11eb-1af8-e179b5503070
